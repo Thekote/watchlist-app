2b107d36b3b200f7eb928fd61b21f645ab34e2ee
Guilherme Monteiro <dkglima@gmail.com>
2022-04-08 11:55:03 -0300

    test: refactor test to use described class

--- spec/sidekiq/update_quote_price_job_spec.rb
+++ spec/sidekiq/update_quote_price_job_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe UpdateQuotePriceJob, type: :job do
     it { is_expected.to be_retryable 5 }
     
     it 'queues a job' do
-      UpdateQuotePriceJob.perform_async(asset.symbol)
+      described_class.perform_async(asset.symbol)
 
-      expect(UpdateQuotePriceJob).to have_enqueued_sidekiq_job(asset.symbol)
+      expect(described_class).to have_enqueued_sidekiq_job(asset.symbol)
     end
   end
 end
